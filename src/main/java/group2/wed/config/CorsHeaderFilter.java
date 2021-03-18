f06c66bdb8ea692180f37f2aba0fa8e7b106ef5c
HiMBixD <tuananvip123@gmail.com>
2021-03-18 19:27:48 +0700

    fixcor

--- src/main/java/group2/wed/config/CorsHeaderFilter.java
+++ src/main/java/group2/wed/config/CorsHeaderFilter.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 @Component
 public class CorsHeaderFilter implements Filter {
-
+    @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
         response.setHeader("Access-Control-Allow-Origin", "*");
