99c3c42b8cfc27484ddf6fcc166edc2658cf21d6
HiMBixD <tuananvip123@gmail.com>
2021-03-18 18:53:47 +0700

    fix cross ori

--- src/main/java/group2/wed/config/WedWebSecurityConfig.java
+++ src/main/java/group2/wed/config/WedWebSecurityConfig.java
@@ -21,11 +21,7 @@ public class WedWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .cors()
-                .and()
-                .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
                 .antMatchers("/api/admin/*").hasRole("ADMIN")
